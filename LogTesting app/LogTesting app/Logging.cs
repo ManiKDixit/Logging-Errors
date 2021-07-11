295826f6927fbcd97bc77ee77326bcc3e772940c
ManiKDixit <56781890+ManiKDixit@users.noreply.github.com>
2021-07-11 22:56:27 +0530

    Initial commit

---
+++ LogTesting app/LogTesting app/Logging.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LogTesting_app
+{
+    class Logging
+    {
+        private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void LogError(Exception ex)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            
+            
+                logger.Info("----------------------------------------");
+                logger.Info("Date: " + DateTime.Now.ToString());
+
+            
+
+           
+                while (ex != null)
+                {
+                    logger.Info(ex.GetType().FullName);
+                 
+                    logger.Info("Message :" + ex.Message);
+                    logger.Info("StackTrace :" + ex.StackTrace);
+
+                    ex = ex.InnerException;
+                }
+            
+            
+
+
+
+        }
+    }
+}
---
+++ LogTesting app/LogTesting app/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace LogTesting_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            Logging lg = new Logging();
+            int[] arr = new int[5] { 99 ,98 ,92 ,97 , 95 };
+            for(int i = 0; i < arr.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+                catch(Exception ex)
+                {
+                    lg.LogError(ex);
+                }
+            }
+
+
+
+
+        }
+
+       /* public static void LogError(Exception ex)
+        {
+            
+                string filepath = @"C:\Users\man5\Documents";
+                using (StreamWriter writer = new StreamWriter(filepath, true))
+                {
+                    writer.WriteLine("----------------------------------------");
+                    writer.WriteLine("Date :" + DateTime.Now.ToString());
+                    writer.WriteLine();
+
+                    while (ex != null)
+                    {
+                        writer.WriteLine(ex.GetType().FullName);
+                        writer.WriteLine("Message :" + ex.Message);
+                        writer.WriteLine("StackTrace :" + ex.StackTrace);
+
+                        ex = ex.InnerException;
+                    }
+                }
+            
+
+            
+            } */
+    }
+}
+
+
+//new StreamWriter(filepath, true)
